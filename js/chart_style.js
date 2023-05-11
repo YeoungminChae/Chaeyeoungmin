4fb5f4c4eba1ff3467b8f9cf3a3db0b48cafe107
YeoungminChae <131223498+YeoungminChae@users.noreply.github.com>
2023-05-11 17:16:56 +0900

    Add files via upload

---
+++ js/chart_style.js
@@ -0,0 +1,86 @@
+$(function () {
+	$('.chart1').easyPieChart({
+		barColor: '#006480',//#02caff #006480 #005066
+		trackColor: '#fff',
+		scaleColor: '#fff',
+		lineCap: 'round',
+		lineWidth: 30,
+		size: 200,
+		animate: 2000,
+		onStart: $.noop,
+		onStop: $.noop
+	});
+	$('.chart2').easyPieChart({
+		barColor: '#803e00',//#fe7a00 #803e00 #663100
+		trackColor: '#fff',
+		scaleColor: '#fff',
+		lineCap: 'round',
+		lineWidth: 30,
+		size: 200,
+		animate: 2000,
+		onStart: $.noop,
+		onStop: $.noop
+	});
+
+	// $('.chart2').easyPieChart({
+	// 	barColor: '#1c88c7',
+	// 	trackColor: '#fff',
+	// 	scaleColor: '#ccc',
+	// 	lineCap: 'round',
+	// 	lineWidth: 30,
+	// 	size: 200,
+	// 	animate: 2000,
+	// 	onStart: $.noop,
+	// 	onStop: $.noop
+	// });
+
+	$('.chart3').easyPieChart({
+		barColor: '#700d72',//#ec6cee
+		trackColor: '#fff',
+		scaleColor: '#fff',
+		lineCap: 'round',
+		lineWidth: 30,
+		size: 200,
+		animate: 2000,
+		onStart: $.noop,
+		onStop: $.noop
+	});
+
+	$('.chart4').easyPieChart({
+		barColor: '#800037',//#ff3b90
+		trackColor: '#fff',
+		scaleColor: '#fff',
+		lineCap: 'round',
+		lineWidth: 30,
+		size: 200,
+		animate: 2000,
+		onStart: $.noop,
+		onStop: $.noop
+	});
+
+	$('.chart5').easyPieChart({
+		barColor: '#620a75',//#dd76f3 #620a75
+		trackColor: '#fff',
+		scaleColor: '#fff',
+		lineCap: 'round',
+		lineWidth: 30,
+		size: 200,
+		animate: 2000,
+		onStart: $.noop,
+		onStop: $.noop
+	});
+
+
+	$('.chart6').easyPieChart({
+		barColor: '#460080',//#cd8eff #460080
+		trackColor: '#fff',
+		scaleColor: '#fff',
+		lineCap: 'round',
+		lineWidth: 30,
+		size: 200,
+		animate: 2000,
+		onStart: $.noop,
+		onStop: $.noop
+	});
+});
+
